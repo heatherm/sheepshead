b755b728fa004d007847f98c149bf8dacdecd6d3
Heather Moore <moore.hj@gmail.com>
2012-10-07 11:39:54 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sheepshead::Application.config.secret_token = 'e7b26cc6123aef7e52afcc17f99c5ff3b3836a9430160339dd7f00149d82ddb46fe2fb4ba8b730636870677a9b201991c6d326ad158e94cc02f27087928a4585'
