6b2681d04615d52ccafe735bf96478c7758b227b
Heather Moore <moore.hj@gmail.com>
2012-10-10 22:51:25 -0700

    add card spec

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,15 +1,23 @@
 require 'spec_helper'
 
 describe Card do
-  it "has a suit" do
+  before do
+    @card = Card.new({suit: :clubs, rank: :queen, value: 3, trump: true})
+  end
 
+  it "has a suit" do
+    @card.suit.should == :clubs
   end
 
   it "has a rank" do
-
+    @card.rank.should == :queen
   end
 
   it "has a point value" do
+    @card.value.should == 3
+  end
 
+  it "knows if it is trump" do
+    @card.trump.should == true
   end
 end
