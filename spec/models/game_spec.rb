f2df41243beab3948f7b870fb917922795b9b5ea
Heather Moore <moore.hj@gmail.com>
2012-10-10 21:06:06 -0700

    improve game spec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,11 +2,14 @@ require 'spec_helper'
 
 describe Game do
   describe "relations" do
-    it "players" do
+    it "has players" do
       game = Game.make!
       player = Player.make!
+      player_two = Player.make!
       GamePlayer.make!(game: game, player: player)
-      game.players.count.should == 1
+      GamePlayer.make!(game: game, player: player_two)
+      GamePlayer.make!
+      game.players.count.should == 2
     end
   end
 end
