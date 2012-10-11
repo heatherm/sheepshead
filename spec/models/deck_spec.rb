7a9f5dc4652337031d23f386661bb8cb50c33c83
Heather Moore <moore.hj@gmail.com>
2012-10-10 22:53:54 -0700

    deck has 14 trump

--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe Deck do
+  before do
+    @deck = Deck.new
+  end
   it "has 32 cards" do
-    Deck.new.cards.count.should == 32
+    @deck.cards.count.should == 32
   end
 
   it "has 120 points total" do
-    Deck.new.cards.sum(&:value).should == 120
+    @deck.cards.sum(&:value).should == 120
+  end
+
+  it "has 14 trump" do
+    @deck.cards.select{ |c| c.trump }.count.should == 14
   end
 end
