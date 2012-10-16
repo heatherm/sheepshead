52461fdc33d920a805273decc741c085abebf1e7
Heather Moore <moore.hj@gmail.com>
2012-10-16 00:36:02 -0700

    add more tests around picking

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -52,7 +52,7 @@ describe Player do
       player = Player.make!
       player.hand = [
           Card.new({suit: :clubs, rank: :jack, value: 2, trump: true, trump_rank: 10}),
-          Card.new({suit: :diamonds, rank: :jack, value: 2, trump: true, trump_rank: 11}),
+          Card.new({suit: :diamonds, rank: :jack, value: 2, trump: true, trump_rank: 7}),
           Card.new({suit: :diamonds, rank: :ace, value: 11, trump: true, trump_rank: 6}),
           Card.new({suit: :diamonds, rank: :eight, value: 0, trump: true, trump_rank: 2}),
           Card.new({suit: :spades, rank: :ace, value: 11, trump: false}),
@@ -71,6 +71,32 @@ describe Player do
           Card.new({suit: :diamonds, rank: :seven, value: 0, trump: true, trump_rank: 1}),
           Card.new({suit: :hearts, rank: :ten, value: 10, trump: false}),
           Card.new({suit: :clubs, rank: :ten, value: 10, trump: false})
+      ]
+      player.pick?.should be_true
+    end
+
+    it "should pick Hand 4: Q♣, Q♦, A♦, 10♦, A♥, K♥" do
+      player = Player.make!
+      player.hand = [
+          Card.new({suit: :clubs, rank: :queen, value: 3, trump: true, trump_rank: 14}),
+          Card.new({suit: :diamonds, rank: :queen, value: 3, trump: true, trump_rank: 11}),
+          Card.new({suit: :diamonds, rank: :ace, value: 11, trump: true, trump_rank: 6}),
+          Card.new({suit: :diamonds, rank: :ten, value: 10, trump: true, trump_rank: 5}),
+          Card.new({suit: :hearts, rank: :ace, value: 11, trump: false}),
+          Card.new({suit: :hearts, rank: :king, value: 4, trump: false})
+      ]
+      player.pick?.should be_true
+    end
+
+    it "should pick Hand 5: Q♠, Q♥, J♦, 10♦, 7♦, K♥" do
+      player = Player.make!
+      player.hand = [
+          Card.new({suit: :spades, rank: :queen, value: 3, trump: true, trump_rank: 13}),
+          Card.new({suit: :hearts, rank: :queen, value: 3, trump: true, trump_rank: 12}),
+          Card.new({suit: :diamonds, rank: :jack, value: 2, trump: true, trump_rank: 7}),
+          Card.new({suit: :diamonds, rank: :ten, value: 10, trump: true, trump_rank: 5}),
+          Card.new({suit: :hearts, rank: :ace, value: 11, trump: false}),
+          Card.new({suit: :hearts, rank: :king, value: 4, trump: false})
       ]
       player.pick?.should be_true
     end
