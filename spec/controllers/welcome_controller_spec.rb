9d0ad1c73e4427ccc3af278ae1cf2403221f6df5
Heather Moore <moore.hj@gmail.com>
2012-11-14 20:11:35 -0800

    rehydrate session

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,9 +2,25 @@ require 'spec_helper'
 
 describe WelcomeController do
   describe "show" do
-    it "should set the session" do
-      get :show
-      @request.session[:game_id].should_not be_nil
+    context 'when the user already has a session' do
+      before do
+        @game = Game.make!
+        @request.session[:game_id] = @game.id
+      end
+      it "should rehydrate the game from the session" do
+        get :show
+        assigns(:game).should == @game
+        Game.count.should == 1
+      end
+    end
+
+    context 'when the user is new' do
+      it "should set the session" do
+        get :show
+        @request.session[:game_id].should_not be_nil
+        assigns(:game).should_not be_nil
+        Game.count.should == 1
+      end
     end
   end
 end
