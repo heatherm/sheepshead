efd5463874fedf0ee9ce36b23fb8c29cfe7c2954
Heather Moore <moore.hj@gmail.com>
2013-01-01 10:14:04 -0800

    show area for played cards

--- app/views/game/play.rb
+++ app/views/game/play.rb
@@ -10,6 +10,10 @@ class Views::Game::Play < Views::Layouts::Application
 
   def main
     div.row do
+      h3 "[Played cards will show up here]"
+    end
+
+    div.row do
       div.span5 do
         h3 "Select a card to play. You are the dealer and picker."
         render_card_block(@hand.cards, true)
