e02141f6763a02498bd37bf1753e611a3a977383
Heather Moore <moore.hj@gmail.com>
2013-01-01 12:53:15 -0800

    fix js finders

--- app/assets/javascripts/bury.js
+++ app/assets/javascripts/bury.js
@@ -6,12 +6,12 @@
       card.toggleClass('picked');
       if ($('.picked').length > 2) {
         card.toggleClass('picked');
-        $('.bury h5').text('Select two cards and click \'Bury\'. ' +
+        $('.blind h5').text('Select two cards and click \'Bury\'. ' +
           'Only 2 cards allowed. Deselect if needed.');
       } else {
-        $('.bury h5').text('Select two cards and click \'Bury\'.');
+        $('.blind h5').text('Select two cards and click \'Bury\'.');
         var cardName = card.data("name");
-        var buryList = $('.bury .area');
+        var buryList = $('.blind .area');
         var cardText = '[ ' + cardName + ' ]';
         if (card.hasClass('picked')) {
           buryList.append(cardText);
@@ -31,11 +31,11 @@
       event.preventDefault();
       var pickedCards = $('.picked');
       if (pickedCards.length != 2){
-        $('.bury h5').text('Select two cards and click \'Bury\'. Please select 2 cards.');
+        $('.blind h5').text('Select two cards and click \'Bury\'. Please select 2 cards.');
       } else {
         var pickedIds = _.map(pickedCards, function(card){return $(card).data('id')});
-        $('.bury input.cards').val(pickedIds);
-        $('.bury form').submit();
+        $('.blind input.cards').val(pickedIds);
+        $('.blind form').submit();
       }
     });
   };
